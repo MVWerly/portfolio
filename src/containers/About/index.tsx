34d7b30bb6eadceba628f5d37cf2ab663bf3547b
MVWerly <mv.werly2@gmail.com>
2025-02-22 12:37:52 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/About/index.tsx
@@ -0,0 +1,21 @@
+import Paragraph from "../../components/Paragraph";
+import Title from "../../components/Title";
+import { GithubSection } from "./styles";
+
+const About = () => (
+  <section>
+    <Title fontSize={16} >Sobre</Title>
+    <Paragraph>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Id labore saepe dignissimos aspernatur. Impedit saepe officiis
+      laudantium cumque ducimus hic tempore perspiciatis provident,
+      repellat veniam. Enim a repellendus cumque? Tenetur?
+    </Paragraph>
+    <GithubSection>
+      <img src="https://github-readme-stats.vercel.app/api?username=MVWerly&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=MVWerly&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSection>
+  </section>
+)
+
+export default About
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Title from "../../components/Title"
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/MVWerly.png"/>
+    <Title fontSize={20}>Marcus Vinicius</Title>
+  </aside>
+)
+
+export default Sidebar
